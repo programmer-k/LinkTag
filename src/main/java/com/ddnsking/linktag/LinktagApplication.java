c2ba03295a11425abff9cec182d6ce9945345b5f
programmer-k <programmer-j@naver.com>
2025-07-23 13:49:51 +0900

    Initialize Spring Boot project with basic dependencies

---
+++ src/main/java/com/ddnsking/linktag/LinktagApplication.java
@@ -0,0 +1,13 @@
+package com.ddnsking.linktag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinktagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinktagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddnsking/linktag/LinktagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddnsking.linktag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinktagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
