719902ab97cb0282a1d73625eccb7c9543380bae
programmer-k <programmer-j@naver.com>
2025-07-26 15:37:23 +0900

    Add User entity and UserRepository interface

---
+++ src/main/java/com/ddnsking/linktag/domain/User.java
@@ -0,0 +1,22 @@
+package com.ddnsking.linktag.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
