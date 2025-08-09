86a96fe05be84c4f8aaec8860686f6f7ed230cd5
programmer-k <programmer-j@naver.com>
2025-08-09 20:27:10 +0900

    Add public

--- src/main/java/com/ddnsking/linktag/service/TagService.java
+++ src/main/java/com/ddnsking/linktag/service/TagService.java
@@ -14,20 +14,20 @@ public class TagService {
     private final TagRepository tagRepository;
 
     @Transactional
-    Tag findOrCreate(String tagName) {
+    public Tag findOrCreate(String tagName) {
         return tagRepository.findByName(tagName)
                 .orElseGet(() -> tagRepository.save(new Tag(tagName)));
     }
 
     @Transactional
-    List<Tag> findOrCreateAll(List<String> tagNames) {
+    public List<Tag> findOrCreateAll(List<String> tagNames) {
         return tagNames.stream()
                 .map(this::findOrCreate)
                 .toList();
     }
 
     @Transactional
-    void deleteIfOrphan(Tag tag) {
+    public void deleteIfOrphan(Tag tag) {
         if (tag.getLinks().isEmpty()) {
             tagRepository.delete(tag);
         }
