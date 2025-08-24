27a92d72feccd0a004360cff2a9642be30e8f7e5
programmer-k <programmer-j@naver.com>
2025-08-24 11:32:28 +0900

    Add EntityGraph annotation

--- src/main/java/com/ddnsking/linktag/repository/LinkRepository.java
+++ src/main/java/com/ddnsking/linktag/repository/LinkRepository.java
@@ -1,10 +1,15 @@
 package com.ddnsking.linktag.repository;
 
 import com.ddnsking.linktag.domain.Link;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface LinkRepository extends JpaRepository<Link, Long> {
+    @EntityGraph(attributePaths = {"tags", "createdBy"})
+    List<Link> findAll();
+
+    @EntityGraph(attributePaths = {"tags", "createdBy"})
     List<Link> findByTags_Name(String tagName);
 }
