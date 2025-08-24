998707d1fadad0750fbe2e5be7e0ca42d86b108f
programmer-k <programmer-j@naver.com>
2025-08-24 10:27:28 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,8 @@ INSERT INTO users (username, password) VALUES ('test', '$2a$10$6gCb/kdLD6uIdPCOQ
 
 INSERT INTO links (title, url, description, created_at, updated_at, user_id, is_public) VALUES ('Google', 'https://www.google.com', 'google', NOW(), NOW(), 1, TRUE);
 INSERT INTO links (title, url, description, created_at, updated_at, user_id, is_public) VALUES ('Naver', 'https://www.naver.com', 'naver', NOW(), NOW(), 1, TRUE);
+INSERT INTO links (title, url, description, created_at, updated_at, user_id, is_public) VALUES ('Amazon', 'https://www.amazon.com', 'amazon', NOW(), NOW(), 1, TRUE);
+INSERT INTO links (title, url, description, created_at, updated_at, user_id, is_public) VALUES ('Coupang', 'https://www.coupang.com', 'coupang', NOW(), NOW(), 1, TRUE);
 
 INSERT INTO tags (name) VALUES ('portal');
 INSERT INTO tags (name) VALUES ('shopping');
@@ -11,3 +13,5 @@ INSERT INTO link_tag (link_id, tag_id) VALUES(1, 2);
 INSERT INTO link_tag (link_id, tag_id) VALUES(1, 3);
 INSERT INTO link_tag (link_id, tag_id) VALUES(2, 1);
 INSERT INTO link_tag (link_id, tag_id) VALUES(2, 2);
+INSERT INTO link_tag (link_id, tag_id) VALUES(3, 2);
+INSERT INTO link_tag (link_id, tag_id) VALUES(4, 2);
